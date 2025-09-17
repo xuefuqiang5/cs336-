bbf011991dd87932e733b3471a213b8df81bf350
薛文琦 <xuewenqi@xuewenqideMacBook-Air.local>
2025-09-17 22:23:21 +0800

    modified bep merge function to no longer use bytes

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -3,28 +3,32 @@ import regex as re
 from collections import Counter
 from itertools import chain
 
-def merge(byte_dict: dict[tuple, int], merge: list[tuple]): 
+def bpe_merge(byte_dict: dict[tuple, int], merge: list[tuple]): 
     pair_dict = Counter(
-        (word[i], word[1+1])
+        (word[i], word[i+1])
         for word, freq in byte_dict.items()
         for i in range(len(word) - 1)
         for _ in range(freq)
-        for pair in [(word[i] + word[i+1])]
+        # for pair in [(word[i] + word[i+1])]
     )    
+    
+    # [print(word) for idx, word in enumerate(pair_dict.items()) if idx < 3]
 
     max_freq = max(pair_dict.values())
     candidates = [pair for pair, freq in pair_dict.items() if freq == max_freq]
     best_tuple = max(candidates, key=lambda p: p[0] + p[1])
-    best_pair = best_tuple[0] + best_tuple[1]
-    merge.append(best_tuple[0], best_tuple[1])
+    best_pair = (best_tuple[0], best_tuple[1])
+    merge.append((best_tuple[0], best_tuple[1]))
+
+    print(f"the best tuple is {best_tuple}")
 
     merged_dict = {}
     for word, freq in byte_dict.items(): 
         merged_tokens, i = [], 0
 
         while i < len(word): 
-            if (i < len(word) - 1) and word[i] + word[i+1] == best_pair: 
-                merged_tokens.append(best_pair)
+            if (i < len(word) - 1) and (word[i], word[i+1]) == best_pair: 
+                merged_tokens.append("".join(best_pair))
                 i += 2
             else: 
                 merged_tokens.append(word[i])
@@ -53,27 +57,27 @@ def bpe_tokenizer(
     [print(word) for idx, word in enumerate(pre_train_freq.items()) if idx < 3]
 
     byte_freq = {
-        (tuple(word.encode("utf-8")) if word not in special_tokens else word): freq
+        (tuple(word) if word not in special_tokens else word): freq
         for word, freq in pre_train_freq.items()
     }
     print(type(byte_freq))
     [print(word) for idx, word in enumerate(byte_freq.items()) if idx < 3]
 
-    init_vocab_size = [len(word) for word, _ in byte_freq.items()]
-    init_vocab_size = sum(init_vocab_size)
+    init_vocab = {token for tokens in byte_freq for token in tokens}
+    init_vocab_size = len(init_vocab)
 
     epochs = vocab_size - (init_vocab_size + len(special_tokens))
     merged_dict = byte_freq
-
+    print(f"the vocab_size is {vocab_size}, the init_vocab_size is {init_vocab_size}, the len(special_tokens) = {len(special_tokens)}")
     print(f"the epochs's value is {epochs}")
-    
+
     merge = [()]
 
     for _ in range(epochs): 
-        merged_dict = merge(merged_dict)
+        merged_dict = bpe_merge(merged_dict, merge)
     
     
-    unique_tokens = {token for tokens in merged_dict.key() for token in tokens} 
+    unique_tokens = {token for tokens in merged_dict.keys() for token in tokens} 
     vocab = {idx: token for idx, token in enumerate(unique_tokens)}
 
     return vocab, merge
@@ -97,3 +101,13 @@ special_tokens = [
 
 vocab, merge = bpe_tokenizer(data_path, vocab_size, special_tokens)
 print(type(vocab), type(merge))
+for _ in range(10): 
+    print(vocab.itmes())
+
+# string = "hello, world!"
+# b = string.encode('utf-8')
+# encoded_list = list(b)
+# string_list = list(string)
+# res = bytes([encoded_list[0],  encoded_list[1]]).decode()
+# print(res)
+
